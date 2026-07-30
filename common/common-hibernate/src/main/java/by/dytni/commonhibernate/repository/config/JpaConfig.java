a75e5b267b552ac38c810e5a8921ef3e129f6970
n.gatsyha <nikolajolesko391@gmail.com>
2026-07-31 01:42:01 +0300

    feat(commons): introduce commons

--- common/common-hibernate/src/main/java/by/dytni/commonhibernate/repository/config/JpaConfig.java
+++ common/common-hibernate/src/main/java/by/dytni/commonhibernate/repository/config/JpaConfig.java
@@ -1,9 +1,10 @@
-package by.dytni.auth.config;
+package by.dytni.commonhibernate.repository.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @Configuration
 @EnableJpaAuditing
