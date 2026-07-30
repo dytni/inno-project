91d69f03b6fa4b086df0e1bb02bbf5303cb67c6f
n.gatsyha <nikolajolesko391@gmail.com>
2026-07-31 01:49:32 +0300

    refactoring(UserService): delete unused imports

--- common/common-events/src/main/java/by/dytni/commonevents/config/KafkaProducerConfig.java
+++ common/common-events/src/main/java/by/dytni/commonevents/config/KafkaProducerConfig.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
--- user-service/src/main/java/by/dytni/userservice/UserServiceApplication.java
+++ user-service/src/main/java/by/dytni/userservice/UserServiceApplication.java
@@ -2,10 +2,7 @@ package by.dytni.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-import by.dytni.commonevents.config.KafkaProducerConfig;
-import by.dytni.commonhibernate.repository.config.JpaConfig;
 
 @SpringBootApplication(
         scanBasePackages = {
