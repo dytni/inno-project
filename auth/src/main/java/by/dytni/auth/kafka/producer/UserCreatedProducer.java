73cab5e2f500d3db1329f5cb8860140f63d1eb0b
n.gatsyha <nikolajolesko391@gmail.com>
2026-09-09 13:11:50 +0300

    refactor(kafka): update kafka config for eop

--- auth/src/main/java/by/dytni/auth/kafka/producer/UserCreatedProducer.java
+++ auth/src/main/java/by/dytni/auth/kafka/producer/UserCreatedProducer.java
@@ -4,6 +4,7 @@ package by.dytni.auth.kafka.producer;
 import static by.dytni.commonevents.CommonsKafkaConstants.KAFKA_USER_CREATED_TOPIC;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -17,6 +18,7 @@ public class UserCreatedProducer {
     private final Producer producer;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional
     public void send(UserCreatedEvent event) {
         producer.send(event, KAFKA_USER_CREATED_TOPIC);
     }
--- common/common-events/src/main/java/by/dytni/commonevents/config/KafkaConsumerConfig.java
+++ common/common-events/src/main/java/by/dytni/commonevents/config/KafkaConsumerConfig.java
@@ -13,8 +13,10 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
-
+import org.springframework.kafka.transaction.KafkaTransactionManager;
 
 @Configuration
 @EnableKafka
@@ -26,13 +28,13 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
-
         Map<String, Object> config = new HashMap<>();
 
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JacksonJsonDeserializer.class);
         config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        config.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
         JacksonJsonDeserializer<Object> deserializer = new JacksonJsonDeserializer<>();
         deserializer.addTrustedPackages("by.dytni.commonevents.dto");
 
@@ -44,12 +46,24 @@ public class KafkaConsumerConfig {
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object>
-    kafkaListenerContainerFactory(
-            ConsumerFactory<String, Object> consumerFactory) {
+    public ConcurrentKafkaListenerContainerFactory<String, Object>   kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory,
+            KafkaTransactionManager<String, Object> kafkaTransactionManager) {
+
         ConcurrentKafkaListenerContainerFactory<String, Object> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
+
         factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setKafkaAwareTransactionManager(kafkaTransactionManager);
         return factory;
     }
+
+
+    @Bean
+    public KafkaTransactionManager<String, Object> kafkaTransactionManager(
+            ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTransactionManager<>(producerFactory);
+    }
+
+
 }
